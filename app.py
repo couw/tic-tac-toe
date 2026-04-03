aef66608cef65e0b15f183aeb291e757828f2f23
shohei.kobayashi <bnzk122@gmail.com>
2026-04-03 21:03:03 +0900

    refactor: encapsulate tic-tac-toe game logic into a class-based structure

--- app.py
+++ app.py
@@ -1,206 +1,119 @@
 import random
-import sys
 import time
 
-# GameField
-# u1|u2|u3
-# m1|m2|m3
-# l1|l2|l3
+class TicTacToe:
+    def __init__(self):
+        self.board = ["-"] * 9
+        self.crosses = "x"
+        self.noughts = "○"
+        self.win_combinations = [
+            (0, 1, 2), (3, 4, 5), (6, 7, 8),  # Horizontal
+            (0, 3, 6), (1, 4, 7), (2, 5, 8),  # Vertical
+            (0, 4, 8), (2, 4, 6)              # Diagonal
+        ]
 
-gameField = [["-", "-", "-"], ["-", "-", "-"], ["-", "-", "-"]]
-u1 = gameField[0][0]
-u2 = gameField[0][1]
-u3 = gameField[0][2]
-m1 = gameField[1][0]
-m2 = gameField[1][1]
-m3 = gameField[1][2]
-l1 = gameField[2][0]
-l2 = gameField[2][1]
-l3 = gameField[2][2]
-crosses = "x"
-noughts = "○"
+    def display_board(self):
+        print(f"{self.board[0]} {self.board[1]} {self.board[2]}")
+        print(f"{self.board[3]} {self.board[4]} {self.board[5]}")
+        print(f"{self.board[6]} {self.board[7]} {self.board[8]}")
 
-# For judging input values
-player_select_list = ["1", "2"]
-point = 0
-input_number_list = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
-input_item = 0
-turn_action_record = [0]
-j = 0
+    def get_available_moves(self):
+        return [i for i, val in enumerate(self.board) if val == "-"]
 
+    def my_turn(self, player_mark):
+        time.sleep(0.5)
+        print("Your turn.")
+        time.sleep(0.2)
+        print("Please enter the place to put it from 1-9.")
+        user_input = input("> ")
+        while True:
+            if not user_input.isdigit() or int(user_input) < 1 or int(user_input) > 9:
+                print("Please enter only numbers from 1 to 9.")
+                user_input = input("> ")
+                continue
+            
+            move = int(user_input) - 1
+            if move not in self.get_available_moves():
+                print("Already entered place.")
+                user_input = input("> ")
+                continue
+            
+            self.board[move] = player_mark
+            self.display_board()
+            break
 
-def game_view():
-    """Game Field Drawing
+    def enemy_turn(self, enemy_mark):
+        time.sleep(0.5)
+        print("AI turn.")
+        available = self.get_available_moves()
+        move = random.choice(available)
+        self.board[move] = enemy_mark
+        self.display_board()
 
-    Draw the game screen on the CLI.
-    """
-    print(u1, u2, u3)
-    print(m1, m2, m3)
-    print(l1, l2, l3)
+    def check_winner(self, mark):
+        for combo in self.win_combinations:
+            if self.board[combo[0]] == self.board[combo[1]] == self.board[combo[2]] == mark:
+                return True
+        return False
 
+    def is_draw(self):
+        return "-" not in self.board
 
-def my_turn(my_point: int, player_turn: int):
-    """Player Turn Action
+    def play(self):
+        print("Welcome to Tic-tac-toe!")
+        time.sleep(0.5)
+        
+        turn_input = input("Enter 1 for the first move and 2 for the second move:")
+        while turn_input not in ["1", "2"]:
+            print("Please enter 1 or 2.")
+            turn_input = input("> ")
+        turn = int(turn_input)
+            
+        if turn == 1:
+            print("You are the first player.")
+            my_mark = self.crosses
+            enemy_mark = self.noughts
+        else:
+            print("You are the Second player.")
+            my_mark = self.noughts
+            enemy_mark = self.crosses
 
-    A function where the player selects a number, checks if the number is valid, 
-    and then passes the value to the draw function.
+        print("1|2|3")
+        print("4|5|6")
+        print("7|8|9")
+        print("Enter the location and the corresponding number.")
+        time.sleep(1.0)
+        print("----------------")
+        self.display_board()
 
-    Args:
-        my_point: An integer where to mark on the game field.
-        player_turn: An integer indicating the first / second attack.
-    """
-    global j, input_item
-    time.sleep(0.5)
-    print("Your turn.")
-    time.sleep(0.2)
-    print("Please enter the place to put it from 1-9.")
-    while my_point in turn_action_record:
-        if j == 1:
-            print("Already entered place.")
-        input_item = input("> ")
-        while input_item not in input_number_list:
-            print("Please enter only numbers from 1 to 9.")
-            input_item = input("> ")
-        my_point = int(input_item)
-        j = 1
-    j = 0
-    if my_point not in turn_action_record:
-        turn_action_record.append(my_point)
-        draw(my_point, player_turn)
+        current_step = 1
+        while True:
+            time.sleep(0.5)
+            
+            is_my_turn = (current_step % 2 == 1 and turn == 1) or (current_step % 2 == 0 and turn == 2)
 
+            if current_step % 2 == 1:
+                print("TURN:" + str((current_step + 1) // 2))
 
-def enemy_turn(enemy_point: int, player_turn: int):
-    """Enemy Turn Action
+            if is_my_turn:
+                self.my_turn(my_mark)
+                if self.check_winner(my_mark):
+                    time.sleep(0.5)
+                    print(f"{my_mark} WIN")
+                    break
+            else:
+                self.enemy_turn(enemy_mark)
+                if self.check_winner(enemy_mark):
+                    time.sleep(0.5)
+                    print(f"{enemy_mark} WIN")
+                    break
+            
+            if self.is_draw():
+                print("DRAW")
+                break
+                
+            current_step += 1
 
-    A function of game progress on the computer side.
-
-    Args:
-        enemy_point: An integer where to mark on the game field.
-        player_turn: An integer indicating the first / second attack.
-    """
-    time.sleep(0.5)
-    print("AI turn.")
-    while enemy_point in turn_action_record:
-        enemy_point = random.randint(1, 9)
-    if enemy_point not in turn_action_record:
-        turn_action_record.append(enemy_point)
-        draw(enemy_point, player_turn)
-
-
-def draw(point: int, player_turn: int):
-    """Game Action
-
-    Substitute x or ○ in the game field list.
-    In addition, a victory judgment is made.
-
-    Args:
-        point: An integer where to mark on the game field.
-        player_turn: An integer indicating the first / second attack.
-    """
-    global u1, u2, u3, m1, m2, m3, l1, l2, l3
-    global item
-    item = ""
-    # Turn
-    if player_turn == 0:
-        item = crosses
-    elif player_turn == 1:
-        item = noughts
-    # Game Parts Drawing
-    if point == 1:
-        u1 = item
-    elif point == 2:
-        u2 = item
-    elif point == 3:
-        u3 = item
-    elif point == 4:
-        m1 = item
-    elif point == 5:
-        m2 = item
-    elif point == 6:
-        m3 = item
-    elif point == 7:
-        l1 = item
-    elif point == 8:
-        l2 = item
-    elif point == 9:
-        l3 = item
-    else:
-        print("ERROR")
-    game_view()
-    # winning decision
-    if u1 == u2 == u3 == item:
-        win_judge(item)
-    elif m1 == m2 == m3 == item:
-        win_judge(item)
-    elif l1 == l2 == l3 == item:
-        win_judge(item)
-    elif u1 == m1 == l1 == item:
-        win_judge(item)
-    elif u2 == m2 == l2 == item:
-        win_judge(item)
-    elif u3 == m3 == l3 == item:
-        win_judge(item)
-    elif u1 == m2 == l3 == item:
-        win_judge(item)
-    elif u3 == m2 == l1 == item:
-        win_judge(item)
-
-
-# WIN messages
-def win_judge(item: str):
-    """WIN messages
-
-    Display a victory message and end the game.
-
-    Args:
-        item = A character string that stores x or ○.
-    """
-    time.sleep(0.5)
-    if item == crosses:
-        print("x WIN")
-        sys.exit()
-    elif item == noughts:
-        print("○ WIN")
-        sys.exit()
-
-
-# Game Opening
-print("Welcome to Tic-tac-toe!")
-time.sleep(0.5)
-input_item = input("Enter 1 for the first move and 2 for the second move:")
-while input_item not in player_select_list:
-    print("Please enter 1 or 2.")
-    input_item = input("> ")
-turn = int(input_item)
-if turn == 1:
-    print("You are the first player.")
-elif turn == 2:
-    print("You are the Second player.")
-else:
-    print("Please enter the valid value.")
-    sys.exit()
-print("1|2|3")
-print("4|5|6")
-print("7|8|9")
-print("Enter the location and the corresponding number.")
-time.sleep(1.0)
-print("----------------")
-game_view()
-
-# Game progression
-for i in range(1, 6):
-    player_turn = 0
-    time.sleep(0.5)
-    print("TURN:" + str(i))
-    if turn == 1:
-        my_turn(point, player_turn)
-    elif turn == 2:
-        enemy_turn(point, player_turn)
-    if i == 5:
-        print("DRAW")
-        sys.exit()
-    player_turn = 1
-    if turn == 1:
-        enemy_turn(point, player_turn)
-    elif turn == 2:
-        my_turn(point, player_turn)
+if __name__ == "__main__":
+    game = TicTacToe()
+    game.play()
